ffbba19e984921c3458156c18021df114b5ac675
Ronnyz <zhengfaan@163.com>
2022-12-20 23:09:05 +0800

    给测试用例添加用例名称

--- src/test/java/com/github/dreamhead/todo/core/TodoItemServiceTest.java
+++ src/test/java/com/github/dreamhead/todo/core/TodoItemServiceTest.java
@@ -2,6 +2,7 @@ package com.github.dreamhead.todo.core;
 
 import com.google.common.collect.ImmutableList;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -26,6 +27,7 @@ public class TodoItemServiceTest {
     }
 
     @Test
+    @DisplayName("Add todo item")
     public void should_add_todo_item() {
         when(repository.save(any())).then(returnsFirstArg());
         TodoItem item = service.addTodoItem(TodoParameter.of("foo"));
@@ -35,12 +37,14 @@ public class TodoItemServiceTest {
     }
 
     @Test
+    @DisplayName("Add todo item fail")
     public void should_throw_exception_for_null_todo_item() {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> service.addTodoItem(null));
     }
 
     @Test
+    @DisplayName("Mark todo item done")
     public void should_mark_todo_item_as_done() {
         final TodoItem foo = new TodoItem("foo");
         foo.assignIndex(1);
@@ -55,6 +59,7 @@ public class TodoItemServiceTest {
     }
 
     @Test
+    @DisplayName("Mark todo item fail")
     public void should_not_mark_todo_item_for_out_of_scope_index() {
         when(repository.findAll()).thenReturn(ImmutableList.of(new TodoItem("foo")));
         final Optional<TodoItem> todoItem = service.markTodoItemDone(TodoIndexParameter.of(2));
@@ -62,6 +67,7 @@ public class TodoItemServiceTest {
     }
 
     @Test
+    @DisplayName("List all todo item")
     public void should_list_all() {
         final TodoItem item = new TodoItem("foo");
         item.assignIndex(1);
@@ -75,6 +81,7 @@ public class TodoItemServiceTest {
     }
 
     @Test
+    @DisplayName("List none todo item")
     public void should_not_list_without_item() {
         when(repository.findAll()).thenReturn(ImmutableList.of());
 
@@ -83,6 +90,7 @@ public class TodoItemServiceTest {
     }
 
     @Test
+    @DisplayName("List not done todo item")
     public void should_list_all_without_done() {
         final TodoItem doneItem = new TodoItem("foo");
         doneItem.assignIndex(1);
@@ -100,6 +108,7 @@ public class TodoItemServiceTest {
     }
 
     @Test
+    @DisplayName("Not list done todo item")
     public void should_not_list_without_done_item() {
         final TodoItem doneItem = new TodoItem("foo");
         doneItem.assignIndex(1);
